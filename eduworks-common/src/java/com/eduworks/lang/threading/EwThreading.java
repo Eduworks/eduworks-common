791d2b732302eaec477f9ad98a69acb201321a14
Ronald Ray <fritz.ray@eduworks.com>
2016-02-27 09:45:09 -0800

    ForkLimit wasn't strictly being followed.

--- eduworks-common/src/java/com/eduworks/lang/threading/EwThreading.java
+++ eduworks-common/src/java/com/eduworks/lang/threading/EwThreading.java
@@ -251,7 +251,7 @@ public class EwThreading
 		{
 			if (forkSlowly || forkLimit != Integer.MAX_VALUE)
 				while (getTaskCount(getThreadLevel()) > threads || getTaskCount(getThreadLevel()) > forkLimit)
-					EwThreading.sleep(5);
+					EwThreading.sleep(0);
 			final int nextLevel = getThreadLevel() + 1;
 			MyRunnable run = new MyRunnable()
 			{
